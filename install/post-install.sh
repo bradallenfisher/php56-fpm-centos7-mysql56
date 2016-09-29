d9da2a9b058f4302b0026c9532f961bcc334c6e3
Brad Fisher <bradallenfisher@gmail.com>
2016-09-29 09:40:06 -0400

    Update post-install.sh

--- install/post-install.sh
+++ install/post-install.sh
@@ -1,3 +1,13 @@
-composer global require drush/drush:7.*
-echo 'export PATH="$HOME/.config/composer/vendor/bin:$PATH"' >> $HOME/.bashrc
-source $HOME/.bashrc
+# Download latest stable release using the code below or browse to github.com/drush-ops/drush/releases.
+php -r "readfile('https://s3.amazonaws.com/files.drush.org/drush.phar');" > drush
+# Or use our upcoming release: php -r "readfile('https://s3.amazonaws.com/files.drush.org/drush.phar');" > drush
+
+# Test your install.
+php drush core-status
+
+# Make `drush` executable as a command from anywhere. Destination can be anywhere on $PATH.
+chmod +x drush
+sudo mv drush /usr/local/bin
+
+# Optional. Enrich the bash startup file with completion and aliases.
+drush init
