f1ba9e05004f68025328cd42686d74cc43227865
Brad Fisher <bradallenfisher@gmail.com>
2016-12-14 16:03:01 -0500

    Update post-install.sh

--- install/post-install.sh
+++ install/post-install.sh
@@ -1,4 +1,5 @@
 # Install Drush
 composer global require drush/drush:7.*	
 echo 'export PATH="$HOME/.config/composer/vendor/bin:$PATH"' >> $HOME/.bashrc
+echo 'export PATH="$HOME/.composer/vendor/bin:$PATH"' >> $HOME/.bashrc
 source $HOME/.bashrc
